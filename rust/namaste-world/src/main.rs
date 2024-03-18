15288f858791f6741fbeb981ee9ec8948860f5db
Anurag Rajawat <anuragsinghrajawat22@gmail.com>
2024-03-18 20:28:20 +0530

    Rust: Namaste world
    Signed-off-by: Anurag Rajawat <anuragsinghrajawat22@gmail.com>

---
+++ rust/namaste-world/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // println! is known as a macro. A macro is a function that writes code for you; all macros have a ! after them.
+    // You don’t need to worry about remembering to add the ! because the compiler will notice if you don’t.
+    println!("Namaste, world!");
+}
